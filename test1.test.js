61b4655aa37cb30259cf5fbe377213afbcf1794a
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-07-29 05:40:22 -0700

    testing complete

--- test1.test.js
+++
@@ -1,5 +0,0 @@
-const stringLength = require('./test1.js');
-
-test("stringLength should return character counts", () => {
-  expect(stringLength("hey")).toBe(3);
-});
--- test4.js
+++
@@ -1,8 +0,0 @@
-
-function capitalize(string) {
-    const arr = string.split('');
-    arr[0] = arr[0].toUpperCase();
-    return arr.join('');
-}
-
-module.exports = capitalize;
